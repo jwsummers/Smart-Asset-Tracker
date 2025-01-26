6b8e873fbfb6ca46247531ffff85f1485805e2c0
Jason Summers <jwsummers.dev@gmail.com>
2025-01-26 16:57:05 -0500

    Fix navigation spacing

--- smart-asset-tracker-frontend/src/components/NavBar.tsx
+++ smart-asset-tracker-frontend/src/components/NavBar.tsx
@@ -38,13 +38,13 @@ const NavBar = () => {
         <div
           className={`${
             menuOpen ? 'block' : 'hidden'
-          } md:flex items-center space-y-4 md:space-y-0 md:space-x-6 absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-black md:bg-transparent z-10 p-4 md:p-0`}
+          } md:flex md:space-x-6 items-center absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-black md:bg-transparent z-10 p-4 md:p-0`}
         >
-          <ul className='flex flex-col md:flex-row items-center w-full md:w-auto'>
+          <ul className='flex flex-col md:flex-row md:space-x-6 w-full md:w-auto'>
             <li>
               <Link
                 to='/'
-                className='block hover:text-orange py-2 text-center'
+                className='block hover:text-orange py-2 text-center md:text-left'
                 onClick={() => setMenuOpen(false)}
               >
                 Home
@@ -54,7 +54,7 @@ const NavBar = () => {
               <li>
                 <Link
                   to='/dashboard'
-                  className='block hover:text-orange py-2 text-center'
+                  className='block hover:text-orange py-2 text-center md:text-left'
                   onClick={() => setMenuOpen(false)}
                 >
                   Dashboard
@@ -65,7 +65,7 @@ const NavBar = () => {
               <li>
                 <Link
                   to='/login'
-                  className='block hover:text-orange py-2 text-center'
+                  className='block hover:text-orange py-2 text-center md:text-left'
                   onClick={() => setMenuOpen(false)}
                 >
                   Login
@@ -73,14 +73,14 @@ const NavBar = () => {
               </li>
             ) : (
               <>
-                <li className='flex items-center space-x-2 py-2 text-center'>
+                <li className='flex items-center space-x-2 py-2 text-center md:text-left'>
                   <FaUser className='text-orange' />
                   <span className='text-orange'>{displayName}</span>
                 </li>
                 <li>
                   <button
                     onClick={handleLogout}
-                    className='block text-red-500 hover:text-red-700 py-2 text-center'
+                    className='block text-red-500 hover:text-red-700 py-2 text-center md:text-left'
                   >
                     Logout
                   </button>
